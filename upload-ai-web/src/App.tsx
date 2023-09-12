5e7146178227e885d9d720a396d2d4e0242f9cc5
WilliamGrohe <william.grohe@gmail.com>
2023-09-12 00:37:46 -0300

    Creatd first form (upload video) on aside

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,22 +1,24 @@
-import { Github } from "lucide-react";
+import { Github, FileVideo, Upload } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
-
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com ❤ por William Grohe</span>
-          
-          <Separator orientation="vertical" className="h-6"/>
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤ por William Grohe
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
 
           <Button variant={"outline"}>
-            <Github className="w-4 h-4 mr-2"/>
+            <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
@@ -25,24 +27,62 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea 
+            <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Inclua o prompt para a IA..." 
+              placeholder="Inclua o prompt para a IA..."
             />
-            <Textarea 
+            <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Resultado gerado pela IA" 
-              readOnly 
+              placeholder="Resultado gerado pela IA"
+              readOnly
             />
           </div>
 
           <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode usar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+            Lembre-se: você pode usar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
           </p>
         </div>
 
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border border-dashed flex flex-col rounded-md aspect-video cursor-pointer text-sm gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="h-4 w-4" />
+              Selecione um vídeo
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="min-h-20 leading-relaxed"
+                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2"/>
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
-  )
+  );
 }
