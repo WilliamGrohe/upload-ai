8429e80edbc1dba3db851a5e5e4969a750d8147b
WilliamGrohe <william.grohe@gmail.com>
2023-09-18 22:15:58 -0300

    Created image preview of video selected

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -3,34 +3,64 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { ChangeEvent, useMemo, useState } from "react";
 
 export function VideoInputForm() {
-  return(
-  <form className="space-y-6">
-    <label
-      htmlFor="video"
-      className="border border-dashed flex flex-col rounded-md aspect-video cursor-pointer text-sm gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-    >
-      <FileVideo className="h-4 w-4" />
-      Selecione um vídeo
-    </label>
+  const [videoFile, setVideoFile] = useState<File | null>(null);
 
-    <input type="file" id="video" accept="video/mp4" className="sr-only" />
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
 
-    <Separator />
+    if (!files) {
+      return;
+    }
 
-    <div className="space-y-2">
-      <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-      <Textarea
-        id="transcription_prompt"
-        className="h-20 leading-relaxed resize-none"
-        placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-      />
-    </div>
+    const selectedFile = files[0];
 
-    <Button type="submit" className="w-full">
-      Carregar vídeo
-      <Upload className="w-4 h-4 ml-2" />
-    </Button>
-  </form>)
+    setVideoFile(selectedFile)
+  }
+
+  const previewURL = useMemo(()=>{
+    if (!videoFile){
+      return null
+    }
+
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
+  return (
+    <form className="space-y-6">
+      <label
+        htmlFor="video"
+        className="relative border border-dashed flex flex-col rounded-md aspect-video cursor-pointer text-sm gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+      >
+        {previewURL ? (
+          <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0"/>
+        ) : (
+          <>
+            <FileVideo className="h-4 w-4" />
+            Selecione um vídeo
+          </>
+        )}
+      </label>
+
+      <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected}/>
+
+      <Separator />
+
+      <div className="space-y-2">
+        <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+        <Textarea
+          id="transcription_prompt"
+          className="h-20 leading-relaxed resize-none"
+          placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+        />
+      </div>
+
+      <Button type="submit" className="w-full">
+        Carregar vídeo
+        <Upload className="w-4 h-4 ml-2" />
+      </Button>
+    </form>
+  );
 }
