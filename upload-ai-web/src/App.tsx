3b752cf2e5093e6749a02e9644005e725e39719a
WilliamGrohe <william.grohe@gmail.com>
2023-09-25 23:52:51 -0300

    Set temperature slider

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -7,8 +7,12 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+
+
   function handlePromptSelected(template: string) {
     console.log(template)
   }
@@ -89,6 +93,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={val => setTemperature(val[0])}
                />
 
               <span className="text-muted-foreground text-xs italic block leading-relaxed">Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.</span>
