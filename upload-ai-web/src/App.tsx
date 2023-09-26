1322e97907274cffabea78239da5c342a2a44c2b
WilliamGrohe <william.grohe@gmail.com>
2023-09-26 00:24:35 -0300

    Show prompt lines on front-end

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -8,15 +8,23 @@ import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
 import { useState } from "react";
+import { useCompletion } from "ai/react";
 
 export function App() {
   const [temperature, setTemperature] = useState(0.5)
   const [videoId, setVideoId] = useState<string | null>(null)
 
-
-  function handlePromptSelected(template: string) {
-    console.log(template)
-  }
+  const {
+    input,
+    setInput,
+    handleInputChange,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    }
+  })
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -43,6 +51,8 @@ export function App() {
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
@@ -57,7 +67,7 @@ export function App() {
             prompt para adicionar o conteúdo da transcrição do vídeo
             selecionado.
           </p>
-        </div>
+        </div> 
 
         <aside className="w-80 space-y-6">
           <VideoInputForm onVideoIdUploaded={setVideoId}/>
@@ -67,7 +77,7 @@ export function App() {
           <form className="space-y-6">
           <div className="space-y-2">
               <Label>Prompt</Label>
-              <PromptSelect onPromptSelected={handlePromptSelected}/>
+              <PromptSelect onPromptSelected={setInput}/>
             </div>
 
             <Separator />
